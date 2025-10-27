a0dbeb0628b7bb4c7c8db21095c2bb487ad5972e
John Rommel Oidem <johnrommeloidem.prf@gmail.com>
2025-10-27 16:12:09 +0800

    Updates

---
+++ shaders/trajectory/vertexShader.glsl
@@ -0,0 +1,12 @@
+#version 300 es
+precision lowp float;
+
+in vec2 a_position;
+uniform mat3 u_transform;
+uniform float u_size;
+
+void main(){
+    vec3 pos = u_transform*vec3(a_position, 1.0);
+    gl_Position = vec4(pos.xy, 0.0, 1.0);
+    gl_PointSize = u_size;
+}
